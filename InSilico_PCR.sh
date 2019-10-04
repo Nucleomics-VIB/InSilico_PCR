e23ab045976f00eae5da1de4d74fe96432da8580
stephane plaisance <stephane.plaisance@vib.be>
2019-10-04 14:06:05 +0200

    Update InSilico_PCR.sh
    revised version with better support for user data and better multithreading control

--- InSilico_PCR.sh
+++ InSilico_PCR.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
 # script: InSilico_PCR.sh
-# Stephane Plaisance VIB-NC September-18-2019 v1.0
 
-# script: InSilico_PCR.sh
-# Stephane Plaisance VIB-NC September-18-2019 v1.0
+# required:
 # conda with -f environment imported
-# requires: bbmap, bioawk, R (computing percentiles)
+# requires: bbmap, bioawk R (computing percentiles)
+#
+# Stephane Plaisance VIB-NC September-18-2019 v1.0
+# version 1.1; 2019-10-04
+# better handling of input file
+# better handling of parallel jobs and threads
 #
 # visit our Git: https://github.com/Nucleomics-VIB
-
-# run once
-# create a conda env to install the required apps
+#
+# required once: create a conda env to install the required apps
 # conda env create -f environment.yaml
-
 # adapt next line to point to the right conda.sh init script
 # see conda activate script for details
 source /etc/profile.d/conda.sh
-conda activate InSilico_PCR
+conda activate InSilico_PCR || \
+	( echo "# the conda environment 'InSilico_PCR' was not found on this machine, please read the top part of the script!" \
+	&& exit 1 )
 
-# get mockcommunity data from the cloud
-# https://github.com/LomanLab/mockcommunity
-infile=${1:-"Zymo-PromethION-EVEN-BB-SN.fq.gz"}
-url=${2:-"https://nanopore.s3.climb.ac.uk"}
+########## start user editable region ##############
 
-# extract string for output names
-name=$(basename ${infile%\.fq\.gz})
+# REM: the workflow can be restarted by deleting all log files after a given step
 
 # speed-up
 thr=80
@@ -33,20 +32,29 @@ pigt=8
 mem="8g"
 
 # extract reads corresponding to the 16S PCR
-forwardp="AGAGTTTGATCMTGGCTCAG"
-forwardl="27F"
-reversep="CGGTWACCTTGTTACGACTT"
-reversel="1492Rw"
+# Long ONT amplicon
+#forwardp="AGAGTTTGATCMTGGCTCAG"
+#forwardl="27F"
+#reversep="CGGTWACCTTGTTACGACTT"
+#reversel="1492Rw"
 
+# typical V4 amplicon
 #forwardp="GACTCCTACGGGAGGCWGCAG"
 #forwardl="337F"
 #reversep="GACTACHVGGGTATCTAATCC"
 #reversel="805R"
 
+# hybrid amplicon, a bit longer
 #forwardp="GTGYCAGCMGCCGCGGTAA"
 #forwardl="515FB"
 #reversep="CGGTWACCTTGTTACGACTT"
 #reversel="1492Rw"
+
+# 4.4kb +/-full-length rRNA amplicon 
+forwardp="AGRGTTYGATYMTGGCTCAG"
+forwardl="ncec_16S_8F_v7"
+reversep="CGACATCGAGGTGCCAAAC"
+reversel="ncec_23S_2490R_v7"
 
 # be stringent to avoid noisy reads
 cut=0.8
@@ -54,21 +62,46 @@ cut=0.8
 # split in 500k read bins and zip
 lines=2000000
 
-# minimum expected amplicon length
-# replaced by calculating the 1% percentile from the results 
-# in order to exclude very short reads
-# minl=1000
-# exclude the 1% shortest reads, change to 0 to disable filtering
-filterperc=0.01
+# quality phred scale of your data (for demo data it is 33)
+qual=33
 
+# expected amplicon size limits, set to 10 and 10000 by default
+# adjust if you notice that the sequence extraction has unwanted tail(s)
+readminlen=10
+readmaxlen=100000
+
+######## end of user editable region ############
+
+# test arguments
+if [ -z "$1" ]; then
+    echo -e "\nUsage: $0 -d (for demo) 
+     or 
+       $0 <path to a local fastq.gz file>\n"
+    exit 1
+fi
+
+if [[ "$1" == "-d" ]]; then
+        # get mockcommunity data from the cloud
+        # https://github.com/LomanLab/mockcommunity
+        infile="Zymo-PromethION-EVEN-BB-SN.fq.gz"
+        url="https://nanopore.s3.climb.ac.uk"
+else
+        if [ -f "${1}" ]; then
+        infile=$1
+    else
+        echo "# input file not found!"
+        exit 1
+    fi
+fi
+
+# extract string for output names
+name=$(basename ${infile%\.fq\.gz})
+
+##################
 # prepare folders
 
-# timestamp to support re-running
-ts=$(date +%s)
-
-# Raw data
-data="RawData_${name}"
-mkdir -p ${data}
+# working default to local folder
+data="$(pwd)"
 
 # run logs
 logs="run_logs"
@@ -78,20 +111,20 @@ mkdir -p ${logs}
 split="split_data_${name}"
 mkdir -p ${split}
 
-tmpout="bbmap_out_${ts}"
+tmpout="bbmap_out_${name}_${forwardl}_${reversel}"
 mkdir -p ${tmpout}
 
-#################################
-# download once only
+##################################
+# download data for -d (once only)
 
-if [[ ! -f ${logs}/done.gettingdata ]]; then
-  echo "# downloading ${infile} (may take some time!)"
-  cd ${data}
-  wget ${url}/${infile}
-  cd ../
-  touch ${logs}/done.gettingdata}
-else
-  echo "# ${infile} was already downloaded from ${url}"
+if [[ "$1" == "-d" ]]; then
+	if [[ ! -f ${logs}/done.gettingdata ]]; then
+  		echo "# downloading ${infile} (may take some time!)"
+  		wget ${url}/${infile}
+  		touch ${logs}/done.gettingdata}
+	else
+  		echo "# ${infile} was already downloaded from ${url}"
+	fi
 fi
 
 ###########################################
@@ -99,7 +132,7 @@ fi
 
 if [[ ! -f ${logs}/done.splitting ]]; then
   echo "# splitting the data in multiple smaller files and compressing (may take some time!)"
-  zcat ${data}/${infile} | split -a 3 -d -l ${lines} --filter='pigz -p '${pigt}' > $FILE.fq.gz' - ${split}/${name}_ &&
+  zcat ${infile} | split -a 3 -d -l ${lines} --filter='pigz -p '${pigt}' > $FILE.fq.gz' - ${split}/${name}_ &&
     touch  ${logs}/done.splitting
 else
   echo "# splitting already done"
@@ -109,28 +142,36 @@ fi
 # run in BBMap msa.sh in parallel
 #################################
 
-#####################
-# find forward primer
+# compute job number & threads/job
+jobs=$(ls ${split}|wc -l)
+# limit to thr
+if [[ "${jobs}" -gt "${thr}" ]]; then
+	jobs=${thr}
+fi
+jobt=$((${thr}/${jobs}))
 
-if [[ ! -f ${logs}/done.${name}_${forwardl}.${ts} ]]; then
+######################################################
+# find forward primer using a fraction of the threads
+
+if [[ ! -f ${logs}/done.searching.${name}_${forwardl} ]]; then
   echo "# searching for forward primer sequence: ${forwardp} in all files"
   find ${split} -type f -name "${name}_???.fq.gz" -printf '%P\n' |\
     sort -n |\
-    parallel -j ${thr} msa.sh -Xmx${mem} qin=33 in=${split}/{} out=${tmpout}/forward_{}.sam literal="${forwardp}" rcomp=t cutoff=${cut} && \
-    touch ${logs}/done.${name}_${forwardl}.${ts}
+    parallel -j ${jobs} msa.sh -Xmx${mem} threads=${jobt} qin=${qual} in=${split}/{} out=${tmpout}/forward_{}.sam literal="${forwardp}" rcomp=t cutoff=${cut} && \
+    touch ${logs}/done.searching.${name}_${forwardl}
 else
   echo "# forward search already done"
 fi
 
-#####################
-# find reverse primer
+######################################################
+# find reverse primer using a fraction of the threads
 
-if [[ ! -f ${logs}/done.${name}_${reversel}.${ts} ]]; then
+if [[ ! -f ${logs}/done.searching.${name}_${reversel} ]]; then
   echo "# searching for reverse primer sequence: ${reversep} in all files"
   find ${split} -type f -name "${name}_???.fq.gz" -printf '%P\n' |\
     sort -n |\
-    parallel -j ${thr} msa.sh -Xmx${mem} qin=33 in=${split}/{} out=${tmpout}/reverse_{}.sam literal="${reversep}" rcomp=t cutoff=${cut} && \
-    touch ${logs}/done.${name}_${reversel}.${ts}
+    parallel -j ${jobs} msa.sh -Xmx${mem} threads=${jobt} qin=${qual} in=${split}/{} out=${tmpout}/reverse_{}.sam literal="${reversep}" rcomp=t cutoff=${cut} && \
+    touch ${logs}/done.searching.${name}_${reversel}
 else
   echo "# reverse search already done"
 fi
@@ -138,39 +179,30 @@ fi
 ##########################################
 # extract regions with BBMap cutprimers.sh
 
-if [[ ! -f ${logs}/done.cutprimer.${name}_${forwardl}_${reversel}.${ts} ]]; then
+if [[ ! -f ${logs}/done.cutprimer.${name}_${forwardl}_${reversel} ]]; then
 echo "# extracting template sequences between primer matches"
 find ${split} -type f -name "${name}_???.fq.gz" -printf '%P\n' |\
   sort -n |\
-  parallel -j ${thr} cutprimers.sh -Xmx${mem} in=${split}/{} out=${tmpout}/{}_16s.fq sam1=${tmpout}/forward_{}.sam sam2=${tmpout}/reverse_{}.sam include=t fixjunk=t &&\
-  touch ${logs}/done.cutprimer.${name}_${forwardl}_${reversel}.${ts}
+  parallel -j ${thr} cutprimers.sh -Xmx${mem} in=${split}/{} out=${tmpout}/{}_16s.fq sam1=${tmpout}/forward_{}.sam sam2=${tmpout}/reverse_{}.sam include=t fixjunk=t && \
+  touch ${logs}/done.cutprimer.${name}_${forwardl}_${reversel}
 fi
 
-####################################################
-# merge results and keep only reads longer than minl
+###########################################################
+# merge results and keep only reads in amplicon size range
 
-if [[ ! -f ${logs}/done.merging.${name}_${forwardl}_${reversel}.${ts} ]]; then
-
-  echo "# analysing read length distribution"
-  minl="$(find ${tmpout} -type f -name "${name}_???.fq.gz_16s.fq" | sort -n |\
-    xargs cat |\
-    bioawk -c fastx '{if (length($seq)>1) print length($seq)}' |\
-    Rscript -e 'unname(quantile (as.numeric (readLines ("stdin")), probs=c('${filterperc}')))' |\
-    cut -d' ' -f2)"
-  echo "# reads shorter than ${minl} nucleotides are removed during the filtering and merging step"
-  echo "# you can disable filtering by changing the value of filterperc to 0"
-  
+if [[ ! -f ${logs}/done.merging.${name}_${forwardl}_${reversel} ]]; then
+  # clear existing results
   final="${name}_${forwardl}_${reversel}.${ts}.fq.gz"
   cat /dev/null > ${final}
-
-  echo "# filtering and merging results to ${final}"
+  echo "# filtering results at min:${readminlen} and max:${readmaxlen} and merging results to ${final}"
   find ${tmpout} -type f -name "${name}_???.fq.gz_16s.fq" | sort -n |\
     xargs cat |\
-    bioawk -c fastx -v varlen="${minl}" '{if (length($seq)>=varlen) print "@"$name" "$comment"\n"$seq"\n+\n"$qual}' |\
+    bioawk -c fastx -v min="${readminlen}" -v max="${readmaxlen}" \
+    '{if (length($seq)>=min && length($seq)<=max) print "@"$name" "$comment"\n"$seq"\n+\n"$qual}' |\
     bgzip >> ${final} && \
-    touch ${logs}/done.merging.${name}_${forwardl}_${reversel}.${ts}
+    touch ${logs}/done.merging.${name}_${forwardl}_${reversel}
 else
-  echo "# already all done, force redo by deleting the subfolders and or final merge file: ${final}"
+  echo "# merging and filtering already done, force redo by deleting ${logs}/done.merging.${name}_${forwardl}_${reversel}"
 fi
 
 # return to normal
